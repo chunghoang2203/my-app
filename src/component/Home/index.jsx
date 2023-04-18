ead0707a5b56dd57d00e162024e92bb02bd8858b
chunghoang2203 <chunghoangph@gmail.com>
2023-04-18 12:59:52 +0700

    product-item

--- src/component/Home/index.jsx
+++ src/component/Home/index.jsx
@@ -5,20 +5,22 @@ import ProductItem from "../ProductItem";
 
 function Home() {
     return (
-        <div className="main">
-            <div className="grid wide">
-                <div>
-                    <div className="grid-top ">
-                        <h3>Trang chủ</h3>
+        <div className="container">
+            <div className="grid wide">               
+                                                               
+                 <div>
+                    <div className="grid-top">
+                         <h2>Trang chủ</h2>
+                     </div>
+                     <div className="grid-top grid-border-bottom">
+                         <span>phim mới - phim hay hd - top trending - phim bom tấn</span>
                     </div>
-                    <div className="grid-top grid-bottom">
-                        <span>phim bộ</span>
-                    </div>
-                </div>
+                 </div>
 
-                <div className="row  main">
+                <div className="row">
 
-                    <div className="col l-8 m-12 c-12 product-list">
+                    <div className="col l-8 m-12 c-12 product-list">        
+
                         <div className="row small-gutters ">
                             <div className="col l-3 m-3 c-6 item">
                                 <ProductItem />
@@ -35,13 +37,22 @@ function Home() {
                             </div>
                             <div className="col l-3 m-3 c-6 item">
                             <ProductItem />
-                            </div>
-                            
+                            </div>    
                         </div>
                     </div>
 
+                    
+
                     <div className="col l-4 m-12 c-12 sidebar">
-                        <div className="product-sidebar"></div>
+
+                    
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
+                        <div className="product-sidebar item"></div>
                     </div>
                 </div>
             </div>
--- src/component/ProductItem/index.jsx
+++ src/component/ProductItem/index.jsx
@@ -4,15 +4,17 @@ function ProductItem() {
   return (
     <div>
       <a href="/">
-          <div className="product-item">
-            <figure className="product-img">
-                <img src="https://static.tvtropes.org/pmwiki/pub/images/100420.jpg" alt="the avengers"></img>
-            </figure>
-            <span className="product-status">Full hd -Phụ đề</span>
-            <span className="product-episode">Tập API</span>
-            <div className="product-pots-title">
-                <h2>The Avengers</h2>
-                <p>Maver</p>
+          <div className="product">
+            <div className="product-item">
+              <figure className="product-img">
+                  <img src="https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg" alt="the avengers"></img>
+              </figure>
+              <span className="product-status">Full hd -Phụ đề</span>
+              <span className="product-episode">Tập API</span>
+              <div className="product-pots-title">
+                  <h2>The Avengers</h2>
+                  <p>Maver</p>
+              </div>
             </div>
           </div>
       </a>
